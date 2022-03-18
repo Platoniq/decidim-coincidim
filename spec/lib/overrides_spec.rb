66a3955d1cf9b01d96a17bf4c89da1749a3e6cfe
Agusti B.R <hola@agusti.cat>
2022-03-18 08:59:56 +0100

    fix(emails): styles

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -19,6 +19,7 @@ checksums = [
       "/app/views/layouts/decidim/_head_extra.html.erb" => "1b8237357754cf519f4e418135f78440",
       "/app/views/layouts/decidim/_logo.html.erb" => "66a4068d3c7cf10fdaf799806c2437fc",
       "/app/views/layouts/decidim/_mini_footer.html.erb" => "5a842f3e880f24f49789ee2f72d96f60",
+      "/app/views/layouts/decidim/mailer.html.erb" => "0c7804de08649c8d3c55c117005e51c9",
       # validators
       "/app/validators/etiquette_validator.rb" => "f7a4a652005385a994208f1ab41c4f08"
     }
